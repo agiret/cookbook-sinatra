06e87b6457c7501799c4169367130748ead3cae6
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-01-22 17:42:50 +0100

    Sinatra app Decouverte

--- app.rb
+++ app.rb
@@ -7,6 +7,20 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+# get '/' do
+#   erb :index
+# end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
 get '/' do
-  'Hello world!'
+  @usernames = [ 'ssaunier', 'Papillard' ]
+  erb :index
 end
